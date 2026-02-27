35c5045016f3a5cf4865ae3cb430814c5f0c18b3
John James Libron <jlibron@cainc.com>
2026-02-27 21:04:40 +0800

    Add association for users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_24_100515) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_27_125119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "cars", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.string "make"
     t.string "model"
-    t.datetime "updated_at", null: false
     t.integer "year"
+    t.datetime "updated_at", null: false
+    t.datetime "created_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", null: false
     t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
